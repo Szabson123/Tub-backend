d0ee147fef09c48402d6b2308a506825571399f2
DESKTOP-TN3VE9B\szymo <szymonzaba75@gmail.com>
2024-07-13 15:37:02 +0200

    static fixed

--- Balie_Sauny/settings.py
+++ Balie_Sauny/settings.py
@@ -165,7 +165,7 @@ CORS_ALLOWED_ORIGINS = [
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 # Media files (Uploaded files)
 MEDIA_URL = '/media/'
